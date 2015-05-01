e534a43e70188c73e6771b611a424965f930c4bd
David Abel <drabelpdx@gmail.com>
2015-05-01 09:45:45 -0700

    Adds the all and save methods

--- lib/word.rb
+++ lib/word.rb
@@ -1,4 +1,5 @@
 class Word
+  @@words = []
 
   define_method(:initialize) do |name|
     @name = name
@@ -7,4 +8,13 @@ class Word
   define_method(:name) do
     @name
   end
+
+  define_singleton_method(:all) do
+    @@words
+  end
+
+  define_method(:save) do
+    @@words.push(self)
+  end
+
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -7,3 +7,11 @@ describe('#name') do
     expect(test_word.name()).to(eq("apple"))
   end
 end
+
+describe('#save') do
+  it("adds a word to the array of saved words") do
+    test_word = Word.new("apple")
+    test_word.save()
+    expect(Word.all()).to(eq([test_word]))
+  end
+end
