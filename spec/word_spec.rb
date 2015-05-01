c0fdd18d09aec0c779feb2612a7ecad905a7c673
David Abel <drabelpdx@gmail.com>
2015-05-01 10:52:15 -0700

    Adds get definition and oed methods to Word class

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,35 +1,45 @@
-# require('rspec')
-# require('word')
-#
-# describe('#name') do
-#   it("returns the name of the word") do
-#     test_word = Word.new("apple")
-#     expect(test_word.name()).to(eq("apple"))
-#   end
-# end
-#
-# describe('#save') do
-#   it("adds a word to the array of saved words") do
-#     test_word = Word.new("apple")
-#     test_word.save()
-#     expect(Word.all()).to(eq([test_word]))
-#   end
-# end
-#
-# describe('.clear') do
-#   it("empties out all the saved words") do
-#     Word.new("apple").save()
-#     Word.clear()
-#     expect(Word.all()).to(eq([]))
-#   end
-# end
-#
-# describe('.find') do
-#   it("returns a word by its id number") do
-#     test_word = Word.new("apple")
-#     test_word.save()
-#     test_word2 = Word.new("dog")
-#     test_word.save()
-#     expect(Word.find(test_word.id())).to(eq(test_word))
-#   end
-# end
+require('rspec')
+require('word')
+require('definition')
+
+describe('#name') do
+  it("returns the name of the word") do
+    test_word = Word.new("apple")
+    expect(test_word.name()).to(eq("apple"))
+  end
+end
+
+describe('#save') do
+  it("adds a word to the array of saved words") do
+    test_word = Word.new("apple")
+    test_word.save()
+    expect(Word.all()).to(eq([test_word]))
+  end
+end
+
+describe('.clear') do
+  it("empties out all the saved words") do
+    Word.new("apple").save()
+    Word.clear()
+    expect(Word.all()).to(eq([]))
+  end
+end
+
+describe('.find') do
+  it("returns a word by its id number") do
+    test_word = Word.new("apple")
+    test_word.save()
+    test_word2 = Word.new("dog")
+    test_word.save()
+    expect(Word.find(test_word.id())).to(eq(test_word))
+  end
+end
+
+describe('#add_definition') do
+  it("adds a new definition to a word") do
+    test_word = Word.new("apple")
+    test_definition = Definition.new("a fruit")
+    test_word.add_definition(test_definition)
+    expect(test_word.oed()).to(eq([test_definition]))
+  end
+end
